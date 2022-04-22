b7be789d530668a7b1ebe1941555524636935ce0
Hua Luo <luohuazju@gmail.com>
2022-04-22 15:09:26 -0500

    a running starter with docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
+	id("org.openapi.generator") version "5.3.0"
 }
 
 group = "com.sillycat"
@@ -20,7 +21,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("javax.validation:validation-api:2.0.1.Final")
+	implementation("org.springdoc:springdoc-openapi-data-rest:1.6.7")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
+	implementation("io.github.microutils:kotlin-logging-jvm:2.1.21")
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -40,6 +47,3 @@ tasks.withType<KotlinCompile> {
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
