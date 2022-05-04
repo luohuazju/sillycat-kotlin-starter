153588579d605571c86a8597023ce6751ece0229
Hua Luo <luohuazju@gmail.com>
2022-05-03 22:33:43 -0500

    add the redis codes for kotlin

--- src/main/kotlin/com/sillycat/kotlinstarter/web/PersonController.kt
+++ src/main/kotlin/com/sillycat/kotlinstarter/web/PersonController.kt
@@ -3,15 +3,25 @@ package com.sillycat.kotlinstarter.web
 import com.sillycat.kotlinstarter.model.Message
 import com.sillycat.kotlinstarter.model.Person
 import com.sillycat.kotlinstarter.service.PersonService
+import org.redisson.api.RLock
+import org.redisson.api.RedissonClient
 import org.springframework.web.bind.annotation.*
+import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/persons")
-class PersonController(val personService: PersonService) {
+class PersonController(val personService: PersonService, val redissonClient: RedissonClient) {
 
     @PostMapping
     fun post(@RequestBody person: Person) {
-        personService.savePerson(person)
+        val lock: RLock = redissonClient.getLock("LOCK" + person.id)
+        if(lock.tryLock(100, 10, TimeUnit.SECONDS)) {
+            try {
+                personService.savePerson(person)
+            } finally {
+                lock.unlock()
+            }
+        }
     }
 
 }
